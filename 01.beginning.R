86c638b5bbde74b8d17e7064ac7777ba1344f935
Sara-inblue <146854835+Sara-inblue@users.noreply.github.com>
2023-10-03 17:47:28 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -17,3 +17,29 @@ final
 
 final^2
 
+# Array
+sophi <- c(10, 20, 30, 50, 70) # c indicates a function
+sophi
+
+paula <- c(100, 500, 600, 1000, 2000)
+paula
+
+plot(paula, sophi)
+
+# change the labels in the plot
+plot(paula, sophi, xlab="number of people", ylab="microplastics")
+
+#or
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics)
+
+# change point symbols in R "point character" pch
+plot(people, microplastics, pch=19)
+
+# increase/decrease the dimension of the points "character exaggeration"
+plot(people, microplastics, cex=2)
+
+# change the color
+plot(people, microplastics, col="blue")  #colors are stored with " "

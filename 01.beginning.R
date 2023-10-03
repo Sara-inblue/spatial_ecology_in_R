cc510367b3cc416ae3fc4e0436bd7829a35fccdd
Sara-inblue <146854835+Sara-inblue@users.noreply.github.com>
2023-10-03 17:04:17 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,19 @@
+# R as a calculator
+2 + 3
+
+# Assign to an object
+zima <- 2 + 3          # in some languages you use = to assign
+zima
+
+duccio <- 5 + 3
+duccio
+
+zima * duccio
+
+# or
+
+final <- zima * duccio
+final
+
+final^2
+

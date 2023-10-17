1662eaeed7df5ec7cd99edfc5758425debc6aa8f
Sara-inblue <146854835+Sara-inblue@users.noreply.github.com>
2023-10-17 17:27:01 +0200

    Update 04.communities_multivar.R

--- 04.communities_multivar.R
+++ 04.communities_multivar.R
@@ -2,3 +2,9 @@
 
 install.packages("vegan)
 libray(vegan)
+
+# going into the field we take 3 samples and count the number of individuals for 3 different species 
+# we use the number of individuals to plot the species on a graph
+
+# since we cannot create plots with more than 3-d (our mind cannot conceive it) we use multivariate analysis
+# maybe in one of the dimensions we cannot see some variables that we see as separate are connected

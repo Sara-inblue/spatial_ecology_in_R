59933bd9496e3ff7321df24c73c86e945a008eab
Sara-inblue <146854835+Sara-inblue@users.noreply.github.com>
2023-10-19 16:55:53 +0200

    Update 04.communities_multivar.R

--- 04.communities_multivar.R
+++ 04.communities_multivar.R
@@ -1,8 +1,5 @@
 # species prefer to go for partition (divion of resources) instead of competition
 
-install.packages("vegan)
-libray(vegan)
-
 # going into the field we take 3 samples and count the number of individuals for 3 different species 
 # we use the number of individuals to plot the species on a graph
 
@@ -14,3 +11,38 @@ libray(vegan)
 
 install.packages("overlap")
 library(overlap)
+
+install.packages("vegan")   # vegetation analysis
+library(vegan)
+
+data(dune)   # matrix that contains the number of individuals for different species
+dune
+
+head(dune)  # it shows the first 6 rows of the dataset
+tail(dune)  # it shows you the last 3 rows of the dataset
+
+# decorana: detrended correspondence (how different species are correspondent with other) analysis
+ord <- decorana(dune)
+ord
+
+# we are interested in the lenght of the 1^ axes (indicates the range)
+ldc1 <- 3.7004   # yo could also use = because they are actually equivalent
+ldc2 <- 3.1166
+ldc3 <- 1.30055
+ldc4 <- 1.47888
+
+total = ldc1 + ldc2 + ldc3 + ldc4
+
+plcd1 = ldc1 * 100 / total
+plcd2 = ldc2 * 100 / total
+plcd3 = ldc3 * 100 / total
+plcd4 = ldc4 * 100 / total
+
+plcd1
+plcd2
+
+plcd1 + plcd2    # they alone are retaining around 70% of variabilty
+
+# final multivariate space
+# it allows you to describe different environments in you dataset
+plot(ord)

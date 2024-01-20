a2fae8ae21585b386835bc1e3709e258f5d9a770
Sara-inblue <146854835+Sara-inblue@users.noreply.github.com>
2024-01-20 18:27:16 +0100

    Update Code_highlights_pt1.R

--- Code_highlights_pt1.R
+++ Code_highlights_pt1.R
@@ -1,5 +1,9 @@
 library(sf)
+
 Programme_BioObs <- read.csv("Programme_BioObs.csv")
 
-Programme_BioObs_spat <- st_as_sf(Programme_BioObs, coords = c("decimalLongitude", "decimalLatitude"),
-                          crs = crs(Med_present_SST))
+
+
+Programme_BioObs_spat <- st_as_sf(Programme_BioObs,
+                                  coords = c("decimalLongitude", "decimalLatitude"),
+                                  crs = crs(Med_present_SST))
